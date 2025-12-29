80970f734f5eb8e7867b5e756f454747fe9afb1a
antonovvova201419-jpg <antonovvova201419@gmail.com>
2025-12-30 01:33:15 +0200

    Simplify get.js to return a static response

--- api/get.js
+++ api/get.js
@@ -1,11 +1,3 @@
-import { createClient } from '@upstash/redis';
-
-const redis = createClient({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
-
-export default async function handler(req, res) {
-  const response = await redis.get('custom_response');
-  res.status(200).send(response || 'no response set');
+export default function handler(req, res) {
+  res.status(200).send('12');
 }
